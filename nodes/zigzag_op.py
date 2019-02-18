798887a601ca7af57b002b296a60c7bd26d01998
Roberto Tron <tron@bu.edu>
2019-02-18 06:37:41 -0500

    Updated zigzag_op

--- nodes/zigzag_op.py
+++ nodes/zigzag_op.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-
-## Node that outputs a sequence of forward-moving arches
+""" Node that outputs a sequence of forward-moving arches """
 
 import rospy
 from geometry_msgs.msg import Twist
 
 
-class ZigZagCommander():
+class ZigZagCommander(object):
+    """
+    Publishes Twist commands on robot_twist topic corresponding to a
+    sequence of forward-moving arches
+    """
+
     def __init__(self):
-        self.pub = rospy.Publisher('motor_vel', Twist, queue_size=10)
+        self.pub = rospy.Publisher('robot_twist', Twist, queue_size=10)
         self.count = 0
-        self.nbActions = 2
+        self.nb_actions = 2
 
     def send(self):
+        """ Publishes a new Twist command """
         #Init message. It is a 3-D Twist, but we will use only two fields
         msg = Twist()
 
@@ -24,23 +29,24 @@ class ZigZagCommander():
             msg.angular.z = 0.5
 
         #Update count
-        self.count = (self.count + 1) % self.nbActions
+        self.count = (self.count + 1) % self.nb_actions
 
         #Publish
         self.pub.publish(msg)
 
 
-def zigzag():
-    rospy.init_node('zigzag_twist')
+def main():
+    """ Main ROS loop """
+    rospy.init_node('zigzag_op')
     #Set rate to use (in Hz)
     rate = rospy.Rate(1)
-    zo = ZigZagCommander()
+    zzc = ZigZagCommander()
     while not rospy.is_shutdown():
         #Talk
-        zo.send()
+        zzc.send()
         #Wait until it is done
         rate.sleep()
 
 
 if __name__ == '__main__':
-    zigzag()
+    main()
