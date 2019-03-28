812a613530090fbde273c85132a06a7a30fd3b2f
Roberto Tron <tron@bu.edu>
2019-03-28 10:51:23 -0400

    Added images of the president.

--- nodes/image_repeater.py
+++ nodes/image_repeater.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
-
-''' 
+'''
 Simple node that repeats images from a compressed topic to a non-compressed one.
 '''
+
 import rospy
 import cv2
 from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge
 
+
 class ImageRepeater():
     def __init__(self):
-        rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, callback=self.callback, queue_size=1, buff_size=2**18) #the buff_size=2**18 avoids delays due to the queue buffer being too small for images
+        rospy.Subscriber(
+            'raspicam_node/image/compressed',
+            CompressedImage,
+            callback=self.callback,
+            queue_size=1,
+            buff_size=2**18
+        )  #the buff_size=2**18 avoids delays due to the queue buffer being too small for images
         self.pub = rospy.Publisher('image_repeated', Image, queue_size=1)
         self.bridge = CvBridge()
-        
-    def callback(self,msg):
+
+    def callback(self, msg):
         # Convert ros image into a cv2 image (we cannot use bridge because topic is compressed)
         np_arr = np.fromstring(msg.data, np.uint8)
         img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
@@ -24,7 +31,8 @@ class ImageRepeater():
         img_processed = img.copy()
         self.pub.publish(self.bridge.cv2_to_imgmsg(img_processed, "bgr8"))
 
+
 if __name__ == '__main__':
     rospy.init_node('image_repeater')
-    ir=ImageRepeater()
+    ir = ImageRepeater()
     rospy.spin()
